9c7efa8cd5f5c0e4afb59c4a5325865e356f5839
xCandy13 <derrick_ngo@yahoo.com>
2023-02-27 12:57:04 -0600

    changed data_analysis page to allow for categorical selection

--- static/js/data_analysis.js
+++ static/js/data_analysis.js
@@ -1,13 +1,49 @@
 
-const cityStats = {
-    "CITY": "../img/Log Price.png", // CITY
-    "CAPITA": "../img/", // CAPITA
-    "WEATHER": "../img/", // WEATHER
-    "CRIME": "../img/", // CRIME
-};
 
-function updatecityStat() {
+function updateGraphs() {
     var image = document.getElementById("cityStat");
-
-    image.src = cityStats[document.getElementById("citySelector").value];
+    if (document.getElementById("dataSelector").value == "CITY") {
+        document.getElementById("graphs").innerHTML = `
+        <img src="/img/Log Price.png">
+        <img src="/img/Price per Sqft vs U.S. News Overall Score.png">
+        <img src="/img/Price per sqft.png">
+        `;
+    }
+    else if (document.getElementById("dataSelector").value == "CAPITA") {
+        document.getElementById("graphs").innerHTML = `
+        <img src="/img/Price per Sqft vs Density.png">
+        <img src="/img/Price per Sqft vs Listings per 1k Capita.png">
+        <img src="/img/Price per Sqft vs Per_Capita_Income_2020.png">
+        `;
+    }
+    else if (document.getElementById("dataSelector").value == "WEATHER") {
+        document.getElementById("graphs").innerHTML = `
+        <img src="/img/Price per Sqft vs Highest Monthly Avg High Temp.png">
+        <img src="/img/Price per Sqft vs Lowest Monthly Avg High Temp.png">
+        <img src="/img/Price per Sqft vs Avg Annual Rainfall.png">
+        <img src="/img/Price per Sqft vs Price per Sqft vs % Water.png">
+        `;
+    }
+    else if (document.getElementById("dataSelector").value == "CRIME") {
+        document.getElementById("graphs").innerHTML = `
+        <img src="/img/Price per Sqft vs Violent Crime Rate.png">
+        <img src="/img/Price per Sqft vs Property Crime Rate.png">
+        `;
+    }
+    else {
+        document.getElementById("graphs").innerHTML = `
+        <img src="/img/Log Price.png">
+        <img src="/img/Price per Sqft vs Avg Annual Rainfall.png">
+        <img src="/img/Price per Sqft vs Density.png">
+        <img src="/img/Price per Sqft vs Highest Monthly Avg High Temp.png">
+        <img src="/img/Price per Sqft vs Listings per 1k Capita.png">
+        <img src="/img/Price per Sqft vs Lowest Monthly Avg High Temp.png">
+        <img src="/img/Price per Sqft vs Price per Sqft vs % Water.png">
+        <img src="/img/Price per Sqft vs Per_Capita_Income_2020.png">
+        <img src="/img/Price per Sqft vs Property Crime Rate.png">
+        <img src="/img/Price per Sqft vs U.S. News Overall Score.png">
+        <img src="/img/Price per Sqft vs Violent Crime Rate.png">
+        <img src="/img/Price per sqft.png">
+        `
+    }
 }
